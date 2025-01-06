c76b2adf846c8dde62186677ae50545f762f7afb
Rohit jha <rohit.jha@medfin.in>
2025-01-07 01:38:37 +0530

    adds keyboard naviagation

--- src/components/Pages/Pages.jsx
+++ src/components/Pages/Pages.jsx
@@ -1,47 +1,62 @@
-import React, {useEffect, useRef} from "react";
+import React, { useEffect, useRef } from "react";
 
-const Pages = ({ pages, setCurrentPage,currentPage}) => {
-        const btnRef = useRef(null);
-        useEffect(() => {
-            if (btnRef.current && typeof btnRef.current.scrollIntoView === "function") {
-              btnRef?.current?.scrollIntoView({
+const Pages = ({ pages, setCurrentPage, currentPage }) => {
+    const btnRef = useRef(null);
+    useEffect(() => {
+        if (btnRef.current && typeof btnRef.current.scrollIntoView === "function") {
+            btnRef?.current?.scrollIntoView({
                 behavior: "smooth",
-                block: "center", 
-              });
+                block: "center",
+            });
+            btnRef.current.focus();
+        }
+    }, [currentPage]);
+
+    const handleKeyDown = (event) => {
+        if (event.key === "ArrowLeft") {
+
+            if (currentPage > 1) {
+                setCurrentPage((prev) => prev - 1);
             }
-          }, [currentPage]);
-  return (
-    <footer className="footer-pagination">
-      <button 
-        className="nav-button" 
-        disabled={currentPage==1}
-        onClick={() => setCurrentPage((prev) => prev - 1)}
-        aria-label="Previous Page"
-      >
-        ◀
-      </button>
-      <div className="scrollable-footer">
-        {pages.map((page) => (
-          <button
-            key={page}
-            ref={page==currentPage?btnRef:null}
-            className={`page-button ${currentPage==page?'active':''}`}
-            onClick={() => setCurrentPage(page)}
-          >
-            {page}
-          </button>
-        ))}
-      </div>
-      <button 
-        className="nav-button" 
-        disabled={currentPage==pages.length}
-        onClick={() => setCurrentPage((prev) => prev + 1)}
-        aria-label="Next Page"
-      >
-        ▶
-      </button>
-    </footer>
-  );
+        } else if (event.key === "ArrowRight") {
+
+            if (currentPage < pages.length) {
+                setCurrentPage((prev) => prev + 1);
+            }
+        }
+    };
+    return (
+        <footer className="footer-pagination" onKeyDown={handleKeyDown} tabIndex="0">
+            <button
+                className="nav-button"
+                disabled={currentPage == 1}
+                onClick={() => setCurrentPage((prev) => prev - 1)}
+                aria-label="Previous Page"
+            >
+                ◀
+            </button>
+            <div className="scrollable-footer">
+                {pages.map((page) => (
+                    <button
+                        key={page}
+                        ref={page == currentPage ? btnRef : null}
+                        className={`page-button ${currentPage == page ? 'active' : ''}`}
+                        onClick={() => setCurrentPage(page)}
+                    >
+                        {page}
+                    </button>
+                ))}
+            </div>
+            <button
+                className="nav-button"
+                disabled={currentPage == pages.length}
+                onClick={() => setCurrentPage((prev) => prev + 1)}
+                aria-label="Next Page"
+            >
+                ▶
+            </button>
+        </footer>
+    );
 };
 
 export default Pages;
