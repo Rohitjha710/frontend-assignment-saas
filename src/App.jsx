033124c7e226056efe1cd91aca47bdea17585864
Rohit jha <rohit.jha@medfin.in>
2025-01-07 01:52:35 +0530

    eslint issues fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import './App.css';
 import useFetch from "./hooks/useFetch";
 import { getTotalPages } from "./utils/getTotalPages";
--- src/components/Pages/Pages.jsx
+++ src/components/Pages/Pages.jsx
@@ -29,7 +29,7 @@ const Pages = ({ pages, setCurrentPage, currentPage }) => {
         <footer className="footer-pagination" onKeyDown={handleKeyDown} tabIndex="0">
             <button
                 className="nav-button"
-                disabled={currentPage == 1}
+                disabled={currentPage === 1}
                 onClick={() => setCurrentPage((prev) => prev - 1)}
                 aria-label="Previous Page"
             >
@@ -39,8 +39,8 @@ const Pages = ({ pages, setCurrentPage, currentPage }) => {
                 {pages.map((page) => (
                     <button
                         key={page}
-                        ref={page == currentPage ? btnRef : null}
-                        className={`page-button ${currentPage == page ? 'active' : ''}`}
+                        ref={page === currentPage ? btnRef : null}
+                        className={`page-button ${currentPage === page ? 'active' : ''}`}
                         onClick={() => setCurrentPage(page)}
                     >
                         {page}
@@ -49,7 +49,7 @@ const Pages = ({ pages, setCurrentPage, currentPage }) => {
             </div>
             <button
                 className="nav-button"
-                disabled={currentPage == pages.length}
+                disabled={currentPage === pages.length}
                 onClick={() => setCurrentPage((prev) => prev + 1)}
                 aria-label="Next Page"
             >
